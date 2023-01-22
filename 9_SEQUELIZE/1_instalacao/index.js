bfcc4018d527b737ace8e106785f66c70e052d51
andreprovensi <andreprovensi@gmail.com>
2023-01-22 09:36:04 -0300

    Resgatando Dados

--- 9_SEQUELIZE/1_instalacao/index.js
+++ 9_SEQUELIZE/1_instalacao/index.js
@@ -14,8 +14,36 @@ app.set('view engine', 'handlebars')
 
 app.use(express.static('public'))
 
-app.get('/', (req,res)=>{
-    res.render('home')
+app.get('/users/create', (req,res)=>{
+    res.render('adduser')
+})
+
+app.post('/users/create', async (req,res)=>{
+    const name = req.body.name
+    const occupation = req.body.occupation
+    let newsletter = req.body.newsletter
+
+    if (newsletter === 'on') {
+        newsletter = true
+    }
+    else{
+        newsletter = false
+    }
+
+    console.log(req.body);
+
+    await User.create({name,occupation,newsletter})
+
+    res.redirect('/')
+})
+
+app.get('/', async (req,res)=>{
+    
+    const users = await User.findAll({raw: true})
+
+    console.log(users);
+
+    res.render('home',{users})
 })
 
 conn.sync().then(()=>{
